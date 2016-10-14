55f3b0e54c205da40e45cf860ad2e76d491b6fa0
Angus <angus.cheung@oocl.com>
2016-10-14 13:40:22 +0800

    Change Tables Schema

--- sql/createTable.sql
+++ sql/createTable.sql
@@ -2,11 +2,14 @@
 @'D:\Git\trainingCamp\projectOne\sql\Merchant\CREATE_M_DISH.sql';
 @'D:\Git\trainingCamp\projectOne\sql\Merchant\CREATE_M_DISH_IMG.sql';
 @'D:\Git\trainingCamp\projectOne\sql\Merchant\CREATE_M_MERCHANT.sql';
-@'D:\Git\trainingCamp\projectOne\sql\Merchant\CREATE_M_MERCHANT_CONTACT.sql';
+@'D:\Git\trainingCamp\projectOne\sql\Merchant\CREATE_M_SHOP.sql';
+@'D:\Git\trainingCamp\projectOne\sql\Merchant\CREATE_M_SHOP_CONTACT.sql';
 
 -- Create Merchant Table
 @'D:\Git\trainingCamp\projectOne\sql\Admin\CREATE_A_ADMIN.sql';
 @'D:\Git\trainingCamp\projectOne\sql\Admin\CREATE_A_DISH.sql';
 @'D:\Git\trainingCamp\projectOne\sql\Admin\CREATE_A_DISH_IMG.sql';
 @'D:\Git\trainingCamp\projectOne\sql\Admin\CREATE_A_MERCHANT.sql';
-@'D:\Git\trainingCamp\projectOne\sql\Admin\CREATE_A_MERCHANT_CONTACT.sql';
+@'D:\Git\trainingCamp\projectOne\sql\Admin\CREATE_A_SHOP.sql';
+@'D:\Git\trainingCamp\projectOne\sql\Admin\CREATE_A_SHOP_CONTACT.sql';
+
