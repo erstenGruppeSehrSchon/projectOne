3976602bb6e7c13c7a9b5e75d7a3e5491fc15b61
Charles Fan <charles.s.h.fan@oocl.com>
2016-10-15 00:13:31 +0800

    JMS Listener

---
+++ CommonUtil/src/service/impl/MerchantManagerImpl.java
@@ -0,0 +1,21 @@
+package service.impl;
+
+import dao.MerchantDao;
+import dao.impl.MerchantDaoImpl;
+import object.merchant.AdminMerchant;
+import object.merchant.MeMerchant;
+import service.MerchantManager;
+
+public class MerchantManagerImpl implements MerchantManager {
+	private MerchantDao md = new MerchantDaoImpl();
+	@Override
+	public MeMerchant findMeMerchantById(int id) {
+		return md.findMeMerchantById(id);
+	}
+	@Override
+	public void setAdminMerchant(AdminMerchant adminMerchant) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
