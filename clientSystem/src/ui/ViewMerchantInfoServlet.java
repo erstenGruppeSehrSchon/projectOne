3da5cf6dc121b9666ba3715aaf26b2c11484fb09
Vincent Tso <VINCENT.TSO@oocl.com>
2016-10-14 11:32:15 +0800

    MerchantDao setup

---
+++ clientSystem/src/ui/ViewMerchantInfoServlet.java
@@ -0,0 +1,39 @@
+package ui;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ViewMerchantInfoServlet
+ */
+public class ViewMerchantInfoServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public ViewMerchantInfoServlet() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+          int id = Integer.parseInt(request.getParameter("MerchantId"));
+          
+
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
