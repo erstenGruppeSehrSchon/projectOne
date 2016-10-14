33d40c6050292f0bd1c281f162a6d852a629b21c
Angus <angus.cheung@oocl.com>
2016-10-14 12:58:53 +0800

    Add Captcha Class and ShowCaptcha Servlet

---
+++ merchantSystem/src/service/impl/ShowCaptcha.java
@@ -0,0 +1,22 @@
+package service.impl;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import entity.Captcha;
+
+public class ShowCaptcha extends HttpServlet {
+	
+	private static final long serialVersionUID = 1L; 
+
+    @Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        Captcha captcha = new Captcha();
+        String captchaStrting = captcha.getCertPic(0, 0, response.getOutputStream());
+        request.getSession().setAttribute("captchaStrting", captchaStrting);   
+	}
+
+}
