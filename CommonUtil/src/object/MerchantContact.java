6aa14adb51c2017036892f7bc1000869dcb0bbbf
Charles Fan <charles.s.h.fan@oocl.com>
2016-10-14 11:49:48 +0800

    update common.jar and add object

---
+++ CommonUtil/src/object/MerchantContact.java
@@ -0,0 +1,34 @@
+package object;
+
+public class MerchantContact {
+	private int cid;
+	private int mid;
+	private String type;
+	private String info;
+	public int getCid() {
+		return cid;
+	}
+	public void setCid(int cid) {
+		this.cid = cid;
+	}
+	public int getMid() {
+		return mid;
+	}
+	public void setMid(int mid) {
+		this.mid = mid;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public String getInfo() {
+		return info;
+	}
+	public void setInfo(String info) {
+		this.info = info;
+	}
+	
+	
+}
