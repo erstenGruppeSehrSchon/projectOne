e3b44e4e430495c7b5c9bcf566c51abc8d78e11f
Vincent Tso <VINCENT.TSO@oocl.com>
2016-10-14 14:44:22 +0800

    upodate

--- clientSystem/src/service/impl/ClientViewMerchantDishServiceImpl.java
+++ clientSystem/src/service/impl/ClientViewMerchantDishServiceImpl.java
@@ -4,4 +4,46 @@ import service.ClientViewMerchantDishService;
 
 public class ClientViewMerchantDishServiceImpl implements ClientViewMerchantDishService{
 
+	@Override
+	public int getDishDID() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getImagePath() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getDishPrice() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public boolean isDishAction() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public String getDishType() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getDishName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int getDishMID() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
 }
