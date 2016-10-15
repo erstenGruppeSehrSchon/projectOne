b076d9cbd848863d5607a475c37c3e6e2cd628af
Patrick Chan <patrick.y.f.chan@oocl.com>
2016-10-15 14:57:26 +0800

    Merchant - align status code

--- merchantSystem/src/consts/Consts.java
+++ merchantSystem/src/consts/Consts.java
@@ -2,15 +2,20 @@ package consts;
 
 public class Consts {
 	
-	public static final String ACCEPTED = "ACCEPTED";
-	public static final String PENDING = "PENDING";
-	public static final String REJECTED = "REJECTED";
-	public static final String FROZEN = "FROZEN";
+	public static final String ACCEPTED = "Accepted";
+	public static final String PENDING = "Pending";
+	public static final String REJECTED = "Rejected";
+	public static final String FROZEN = "Frozen";
 	
+	// register
 	public static final String USERNAME_CANNOT_EMPTY = "Username cannot be empty. Please re-enter.";
 	public static final String PASSWORD_CANNOT_EMPTY = "Password cannot be empty. Please re-enter.";
 	public static final String NAME_CANNOT_EMPTY = "Name cannot be empty. Please re-enter.";
 	public static final String GENDER_CANNOT_EMPTY = "Gender cannot be empty. Please re-enter.";
 	public static final String INVALID_DATE = "Invalid date. Please re-enter.";
+	
+	// login
+	public static final String LOGIN_FAILURE = "Login failure. Please login again.";
+	public static final String USER_NOT_EXIST = "Merchant does not exist. Please register first.";
 
 }
