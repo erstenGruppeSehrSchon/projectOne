d59f469632c0beb4bd84a6cc5eaa1d35a48b028e
Charles Fan <charles.s.h.fan@oocl.com>
2016-10-26 16:03:42 +0800

    Admin ack user status

--- Admin/src/controller/AdminController.java
+++ Admin/src/controller/AdminController.java
@@ -2,13 +2,28 @@ package controller;
 
 import java.util.List;
 
+import javax.jms.Connection;
+import javax.jms.ConnectionFactory;
+import javax.jms.Destination;
+import javax.jms.JMSException;
+import javax.jms.MessageProducer;
+import javax.jms.Session;
+import javax.jms.TextMessage;
+
+import org.apache.activemq.ActiveMQConnectionFactory;
+import org.apache.activemq.command.ActiveMQQueue;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.servlet.ModelAndView;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import po.Admin;
 import po.Advertisement;
@@ -71,7 +86,9 @@ public class AdminController {
 	@RequestMapping(value="updateMerchantStatus", method={RequestMethod.POST})
 	@ResponseBody
 	public Merchant updateMerchantStatus(String mid, String status) {
-		return merchantManager.updateMerchantStatus(mid, status);
+		Merchant merchant = merchantManager.updateMerchantStatus(mid, status);
+		sendUpdateStatus(merchant);
+		return merchant;
 	}
 	
 	@RequestMapping(value="getAdvertisementsByStatus", method={RequestMethod.GET})
@@ -89,6 +106,32 @@ public class AdminController {
 		advertisementManager.updateAdvertisementStatus(advId, status);
 	}
 	
-	
+	private void sendUpdateStatus(Merchant merchant){
+		ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+		ConnectionFactory factory = (ActiveMQConnectionFactory)context.getBean("mq");
+		
+//		Destination queue = new ActiveMQQueue("Test01Q");
+		Destination queue = (ActiveMQQueue)context.getBean("statusQueue");
+		Connection con = null;
+		Session session = null;
+		MessageProducer producer = null;
+		try {
+			con = factory.createConnection();
+			con.start();
+			session = con.createSession(false, Session.CLIENT_ACKNOWLEDGE);
+			producer = session.createProducer(queue);
+			ObjectMapper mapper = new ObjectMapper();
+			Merchant m = new Merchant();
+			m.setMid(merchant.getMid());
+			m.setStatus(merchant.getStatus());
+			TextMessage msg = session.createTextMessage(mapper.writeValueAsString(m));
+			producer.send(msg);
+		} catch (JMSException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (JsonProcessingException e){
+			e.printStackTrace();
+		}
+	}
 	
 }
