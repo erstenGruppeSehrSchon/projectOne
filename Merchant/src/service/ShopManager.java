698a8b374090e3c86609dfacb4754b83b6735195
Angus <angus.cheung@oocl.com>
2016-10-27 08:52:50 +0800

    Merchant - Add Shop

--- Merchant/src/service/ShopManager.java
+++ Merchant/src/service/ShopManager.java
@@ -7,7 +7,7 @@ import po.Shop;
 public interface ShopManager {
 
 	public Shop getShopBySid(String sid);
-	public Shop addShop(String mid, String name, String description, String type, String openTime, String closeTime, List<MultipartFile> files);
+	public Shop addShop(String mid, String name, String description, String type, String openTime, String closeTime, String address, String phone, List<MultipartFile> files);
 	public boolean removeShop(String sid);
 	public Shop updateShop(String sid, String name, String description, String type, String openTime, String closeTime, String address, String phone);
 	
